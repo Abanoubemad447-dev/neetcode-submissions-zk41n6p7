70057080f58dd05dd46e4e3c2c61ea0c22529bf2
Abanoub Emad <abanobemad447@gmail.com>
2026-04-29 00:23:01 +0200

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False 
+        countS, countT = {}, {}
+
+        # 2. Since lengths are equal, we can safely use one index for both
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+            
+        # 3. Compare the full maps
+        return countS == countT
